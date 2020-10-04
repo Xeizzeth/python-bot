ab9b2b466b662a8fdea1b4a388dce7988a5dd6fb
Xeizzeth <xeizzeth@gmail.com>
2020-10-05 04:02:40 +0500

    add overridable methods

--- bot/bot.py
+++ bot/bot.py
@@ -1,13 +1,125 @@
 from .base_bot import BaseBot
 
+"""
+Information about your bot:
+
+# Resources and supply
+self.minerals: int
+self.vespene: int
+self.supply_army: int # 0 at game start
+self.supply_workers: int # 12 at game start
+self.supply_cap: int # 14 for zerg, 15 for T and P at game start
+self.supply_used: int # 12 at game start
+self.supply_left: int # 2 for zerg, 3 for T and P at game start
+
+# Units
+self.warp_gate_count: Units # Your warp gate count (only protoss)
+self.idle_worker_count: int # Workers that are doing nothing
+self.army_count: int # Amount of army units
+self.workers: Units # Your workers
+self.larva: Units # Your larva (only zerg)
+self.townhalls: Units # Your townhalls (nexus, hatchery, lair, hive, command center, orbital command, planetary fortress
+self.gas_buildings: Units # Your gas structures (refinery, extractor, assimilator
+self.units: Units # Your units (includes larva and workers)
+self.structures: Units # Your structures (includes townhalls and gas buildings)
+
+# Other information about your bot
+self.race: Race # The race your bot plays. If you chose random, your bot gets assigned a race and the assigned race will be in here (not random)
+self.player_id: int # Your bot id (can be 1 or 2 in a 2 player game)
+# Your spawn location (your first townhall location)
+self.start_location: Point2
+# Location of your main base ramp, and has some information on how to wall the main base as terran bot (see GameInfo)
+self.main_base_ramp: Ramp
+
++++
+
+Information about the enemy player:
+
+# The following contains enemy units and structures inside your units' vision range (including invisible units, but not burrowed units)
+self.enemy_units: Units
+self.enemy_structures: Units
+
+# Enemy spawn locations as a list of Point2 points
+self.enemy_start_locations: List[Point2]
+
+# Enemy units that are inside your sensor tower range
+self.blips: Set[Blip]
+
+# The enemy race. If the enemy chose random, this will stay at random forever
+self.enemy_race: Race
+
++++
+
+Other information:
+
+# Neutral units and structures
+self.mineral_field: Units # All mineral fields on the map
+self.vespene_geyser: Units # All vespene fields, even those that have a gas building on them
+self.resources: Units # Both of the above combined
+self.destructables: Units # All destructable rocks (except the platforms below the main base ramp)
+self.watchtowers: Units # All watch towers on the map (some maps don't have watch towers)
+self.all_units: Units # All units combined: yours, enemy's and neutral
+
+# Locations of possible expansions
+self.expansion_locations: Dict[Point2, Units]
+
+# Game data about units, abilities and upgrades (see game_data.py)
+self.game_data: GameData
+
+# Information about the map: pathing grid, building placement, terrain height, vision and creep are found here (see game_info.py)
+self.game_info: GameInfo
+
+# Other information that gets updated every step (see game_state.py)
+self.state: GameState
+
+# Extra information
+self.realtime: bool # Displays if the game was started in realtime or not. In realtime, your bot only has limited time to execute on_step()
+self.time: float # The current game time in seconds
+self.time_formatted: str # The current game time properly formatted in 'min:sec'
+
++++
+
+
+"""
+
 class Bot(BaseBot):
     async def on_before_start(self):
-        pass
+        print("Init")
 
     async def on_start(self):
-        print("i very well")
+        print("the game has started")
+
+    async def on_end(self, game_result):
+        print("The game has ended", game_result)
 
     async def on_step(self, iteration: int):
         if iteration == 0:
             for worker in self.workers:
                 worker.attack(self.enemy_start_locations[0])
+
+    async def on_building_construction_started(self, building):
+        print("the building has started", building)
+
+    async def on_building_construction_complete(self, building):
+        print("the building completed", building)
+
+    async def on_upgrade_complete(self, upgrade):
+        print("upgrade complete", upgrade)
+
+    async def on_unit_created(self, unit):
+        print("unit created", unit)
+
+    async def on_unit_destroyed(self, unit_tag):
+        print("unit destroyed", unit_tag)
+
+    async def on_unit_took_damage(self, damage_amount):
+        print("unit got damaged", damage_amount)
+
+    async def on_enemy_unit_entered_vision(self, unit):
+        print("unit became visible", unit)
+
+    async def on_enemy_unit_left_vision(self, unit_tag):
+        print("unit left vision", unit_tag)
+
+    async def on_unit_type_changed(self, unit, previous_type):
+        print("unit changed type", unit, previous_type)
