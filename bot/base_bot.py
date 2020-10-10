9cb6b8e086811ce670555c7b60236b1fb53aafb1
Xeizzeth <xeizzeth@gmail.com>
2020-10-11 00:36:50 +0500

    Make text debug drawing, draw basic primitives

--- bot/base_bot.py
+++ bot/base_bot.py
@@ -10,10 +10,53 @@ class BaseBot(sc2.BotAI):
     async def chat(self, message):
         await self.client.chat_send(message, False)
 
-    def draw_sphere(self, position, radius=1):
-        z = self.get_terrain_z_height(position)
-        point = sc2.position.Point3((position.x, position.y, z))
+    def get_xyz(self, point):
+        if isinstance(point, sc2.position.Point3):
+            x, y, z = point
+        elif isinstance(point, sc2.position.Point2):
+            x, y, z = (*point, self.get_terrain_z_height(point))
+        else:
+            raise NotImplementedError("Not implemented for this type")
+
+        return (x, y, z)
+
+    def draw_sphere(self, position, radius=1, color=None):
+        x, y, z = self.get_xyz(position)
+        position = sc2.position.Point3((x, y, z))
+        color = (255, 255, 255) if not color else color
+
         self.client.debug_sphere_out(
-            p=point,
-            r=radius
+            p=position,
+            r=radius,
+            color=color
         )
+
+    def draw_text(self, text, position, color=None, size=12):
+        x, y, z = self.get_xyz(position)
+        position = sc2.position.Point3((x, y, z))
+        color = (255, 255, 255) if not color else color
+
+        self.client.debug_text_world(
+            text=text,
+            pos=position,
+            color=color,
+            size=size
+        )
+
+    def draw_text_info(self, entries, position, x_offset=2.5):
+        x, y, z = self.get_xyz(position)
+
+        range_offset = int(x_offset * 10)
+
+        for y_offset, entry in zip(
+            range(range_offset, -range_offset, -5),
+            entries
+        ):
+            y_offset /= 10
+
+            text_position = sc2.position.Point3((
+                x + x_offset,
+                y + y_offset,
+                z
+            ))
+            self.draw_text(entry, text_position)
--- bot/managers/mining_manager.py
+++ bot/managers/mining_manager.py
@@ -30,4 +30,41 @@ class MiningManager():
         await self.draw_debug_info()
 
     async def draw_debug_info(self):
+        entries = [
+            "one",
+            "two",
+            "three",
+            "four",
+            "five",
+            "six",
+            "seven",
+            "eight",
+            "nine",
+            "ten"
+        ]
+
         self.bot.draw_sphere(self.position, 2.5)
+        self.bot.draw_text_info(entries, self.position)
+
+        for mineral in self.minerals:
+            self.bot.draw_sphere(
+                mineral.position,
+                color=(0, 255, 255)
+            )
+            self.bot.draw_text_info(
+                entries=entries[:2],
+                position=mineral.position,
+                x_offset=0.5
+            )
+
+        for geyser in self.geysers:
+            self.bot.draw_sphere(
+                geyser.position,
+                radius=1.5,
+                color=(0, 255, 0)
+            )
+            self.bot.draw_text_info(
+                entries=entries[:6],
+                position=geyser.position,
+                x_offset=1.5
+            )
